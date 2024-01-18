00afcf598aa2474ec7340d138accd3d04940b3fd
gitKristinaMalaveci <gitkristina.malaveci1@gmail.com>
2024-01-18 13:03:41 +0100

    update project

--- nameApp/models.py
+++ nameApp/models.py
@@ -1,6 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    category_title = models.CharField(max_length=200, null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.category_title}"
+    
 
 class Staff (models.Model):
     staff_name = models.CharField(max_length=200, null=True, blank=True)
@@ -8,6 +14,16 @@ class Staff (models.Model):
     staff_position = models.CharField(max_length=200, null=True, blank=True)
     staff_email = models.EmailField(max_length=200, null=True, blank=True)
     staff_image = models.ImageField(upload_to='staff/', null=True, blank=True)
+    staff_category = models.ForeignKey(Category, on_delete=models.CASCADE, null=True, blank=True)
 
     def __str__(self):
         return f"{self.staff_name} {self.staff_surname}"
+    
+class Contact(models.Model):
+    contact_name = models.CharField(max_length = 30, null=True, blank=True)  
+    contact_surname = models.CharField(max_length = 30, null=True, blank=True)  
+    contact_email = models.EmailField(max_length = 100, null=True, blank=True)   
+    contact_comment = models.TextField(max_length = 1000, null=True, blank=True) 
+
+    def __str__(self):
+        return f"{self.contact_name} {self.contact_surname}"
--- nameApp/views.py
+++ nameApp/views.py
@@ -1,23 +1,56 @@
 from django.shortcuts import render
 from.models import *
+from django.contrib import messages
 
 # Create your views here.
 def home (request):
     staffs= Staff.objects.all()
-    context = {"allStaff":staffs}
+    categories = Category.objects.all()
+    context = {"allStaff":staffs, "infoCategories":categories}
     return render(request, 'home.html', context)
   
 
 def about(request):
     staffs= Staff.objects.all()
-    context = {"allStaff":staffs}
+    categories = Category.objects.all()
+    contacts = Contact.objects.all()
+    context = {"allStaff":staffs, "infoContact":contacts, "infoCategories":categories}
     return render(request, 'about.html', context)
 
 def profile(request, id):
     staff= Staff.objects.get(pk=id)
-    context={"profile":staff}
+    categories = Category.objects.all()
+    context={"profile":staff, "infoCategories":categories}
     return render(request, 'profile.html', context)
 
 def contact(request):
-    return render(request, 'contact.html')
+    categories = Category.objects.all()
+    context={"infoCategories":categories}
+    if request.method == "POST":
+        #marrja e inf nga inputi
 
+        name_contact = request.POST['nameField']
+        surname_contact = request.POST['lastField']
+        email_contact = request.POST['emailField']
+        comment_contact = request.POST['messageField']
+        if name_contact != "" and surname_contact != "" and email_contact != "" and comment_contact != "":
+                
+
+            #ruajtje tek modeli
+
+                Contact(contact_name=name_contact,
+                        contact_surname=surname_contact,
+                        contact_email=email_contact,
+                        contact_comment=comment_contact
+                        ).save()
+                messages.success(request, "Message Sended!")
+        else:
+                messages.error(request, "Message not Sended! Fill all the fields!")    
+    return render(request, 'contact.html', context)
+
+def categoryInfo(request, id):
+     categories = Category.objects.all()
+     category = Category.objects.get(pk = id)
+     elementeCategory = Staff.objects.filter(staff_category=category)
+     context={ "infoCategories":categories, "detailCategory":category, "elementeCategory":elementeCategory}
+     return render(request,"category.html", context)
